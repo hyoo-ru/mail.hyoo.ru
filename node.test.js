2ce8fbd8d4f3cb2998f0fb952246e5aca2cff0ea
nin-jin <nin-jin@users.noreply.github.com>
2023-11-14 07:10:21 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -8299,6 +8299,11 @@ var $;
                     return;
                 document.execCommand('insertText', false, symbol);
             }
+            clickable(next) {
+                if (!this.enabled())
+                    return true;
+                return next ?? false;
+            }
             hover(event) {
                 this.clickable(event.ctrlKey);
             }
@@ -8323,6 +8328,9 @@ var $;
                 return index;
             }
         }
+        __decorate([
+            $mol_mem
+        ], $mol_textarea.prototype, "clickable", null);
         $$.$mol_textarea = $mol_textarea;
     })($$ = $.$$ || ($.$$ = {}));
 })($ || ($ = {}));
